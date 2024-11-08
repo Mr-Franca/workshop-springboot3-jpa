7f64c2d90e055c42f79f2e473ba1817d878f89a8
Alex Silva <alex._.silva@outlook.com>
2024-11-08 12:43:13 -0300

    OrderItem, many to many pt2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,6 +36,9 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Long getId() {
 		return id;
 	}
@@ -67,6 +73,8 @@ public class Order implements Serializable {
 		}
 	}
 
+	
+	
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 
 		this.id = id;
@@ -78,12 +86,18 @@ public class Order implements Serializable {
 	public Order() {
 
 	}
-
+	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
+	
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -1,13 +1,14 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_order_item")
@@ -15,21 +16,26 @@ public class OrderItem implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
-
+	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 
 	private Integer quantity;
 	private Double price;
+	
+	public OrderItem() {
+		
+	}
 
-	public OrderItem(Order order, Product product,Integer quantity, Double price) {
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
 		this.price = price;
-		
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
@@ -37,17 +43,16 @@ public class OrderItem implements Serializable {
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
 	
-	public void setProdutc(Product product) {
+	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
-	public OrderItem() {
-	}
-
+	
+	
 	public Integer getQuantity() {
 		return quantity;
 	}
@@ -66,7 +71,10 @@ public class OrderItem implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -78,7 +86,13 @@ public class OrderItem implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		OrderItem other = (OrderItem) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
+	
+	
 }
